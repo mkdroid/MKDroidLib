c9e13b3ef2b00c86cfe5c98d8a0520440effd072
Koushik Das <koushikworlddas@gmail.com>
2020-03-18 16:13:36 +0530

    first commit

---
+++ lib/build/generated/source/buildConfig/debug/cafe/adriel/androidaudioconverter/BuildConfig.java
@@ -0,0 +1,18 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package cafe.adriel.androidaudioconverter;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "cafe.adriel.androidaudioconverter";
+  /**
+   * @deprecated APPLICATION_ID is misleading in libraries. For the library package name use LIBRARY_PACKAGE_NAME
+   */
+  @Deprecated
+  public static final String APPLICATION_ID = "cafe.adriel.androidaudioconverter";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
